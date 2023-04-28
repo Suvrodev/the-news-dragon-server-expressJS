f456aa27a3586f2f3d15157896212a28111e9454
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-04-28 13:55:20 +0600

    Added News and find them according to news id and category id

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app=express();
 const cors=require('cors')
 
 const categories=require('./Data/categories.json')
+const news=require('./Data/news.json')
+
 const port=process.env.PORT|| 5000
 
 app.use(cors())
@@ -11,8 +13,32 @@ app.get('/',(req,res)=>{
     res.send('Hi Suvrodeb')
 });
 
-app.get('/data',(req,res)=>{
+app.get('/categories',(req,res)=>{
     res.send(categories)
+})
+
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+
+
+
+app.get('/news/:id',(req,res)=>{
+    const id=req.params.id;
+    console.log(id)
+    const selectedNews=news.find(n=>n._id===id)
+    res.send(selectedNews)
+})
+app.get('/categories/:id',(req,res)=>{
+    const id=req.params.id
+    console.log(id)
+    if(id==0){
+        res.send(news)
+    }
+    else{
+        const categoriesNews=news.filter(n=>n.category_id===id);
+        res.send(categoriesNews)
+    }
 })
 
 app.listen(port,()=>{
